a84214d5e91bda448903866a813f6323e72c232c
AliAryaie <a.aryaienejad@mail.sbu.ac.ir>
2022-09-22 14:52:54 +0330

    User Logout

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+import knox.views as knox
 
 from . import views
 
@@ -7,4 +8,6 @@ urlpatterns = [
     path("register", views.UserRegister.as_view()),
     path("login", views.UserLogin.as_view()),
     path("profile", views.UserProfile.as_view()),
+    path("logout", knox.LogoutView.as_view()),
+    path("logoutall", knox.LogoutAllView.as_view()),
 ]
