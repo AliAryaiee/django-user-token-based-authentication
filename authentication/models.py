b600a60f597e7694e64bfc109ac012fa5bd21016
AliAryaie <a.aryaienejad@mail.sbu.ac.ir>
2022-09-22 09:50:19 +0330

    Custom User Model

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """
+        User Account Manger
+    """
+
+    def create_user(self, username: str, phone: str, password=None):
+        """
+            Creates and Saves a User with the Given Username, Mobile Number and Password.
+        """
+        if not username:
+            raise ValueError("Users Must Have an Valid Username!")
+        if not phone:
+            raise ValueError("Users Must Have an Valid Mobile Number!")
+
+        user = self.model(
+            username=username.strip().lower(),
+            phone=phone
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self,  username: str, phone: str, password=None):
+        """
+            Creates and Saves a User with the Given Username, Mobile Number and Password.
+        """
+        user = self.create_user(
+            username,
+            phone,
+            password
+        )
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+        User Account
+    """
+    username = models.CharField(max_length=256)
+    phone = models.CharField(max_length=256)
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["phone"]
+
+    def __str__(self):
+        return self.username
